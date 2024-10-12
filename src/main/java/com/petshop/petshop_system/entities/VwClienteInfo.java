2c0e5b746b0ec4f2d80ff52bb5ab0f111dbf7d6b
ClaraLeticia <claralet1346@gmail.com>
2024-10-12 18:58:35 -0300

    view for cliente info in medvet homepage

---
+++ src/main/java/com/petshop/petshop_system/entities/VwClienteInfo.java
@@ -0,0 +1,24 @@
+package com.petshop.petshop_system.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Immutable;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Immutable
+@Table(name = "vw_cliente_info")
+public class VwClienteInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+
+    private String celular;
+    private String email;
+}
+
---
+++ src/main/java/com/petshop/petshop_system/repositories/VwClienteInfoRepositoy.java
@@ -0,0 +1,8 @@
+package com.petshop.petshop_system.repositories;
+
+import com.petshop.petshop_system.entities.VwClienteInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VwClienteInfoRepositoy extends JpaRepository<VwClienteInfo, Long> {
+
+}

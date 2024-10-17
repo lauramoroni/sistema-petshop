178f3e849f21d15f5592ee799ae3c37ba9ddffed
lauramoroni <moronilaura28@gmail.com>
2024-10-16 21:34:05 -0300

    login and password error message

--- src/main/java/com/petshop/petshop_system/controller/MedVetController.java
+++ src/main/java/com/petshop/petshop_system/controller/MedVetController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.petshop.petshop_system.entities.Animal;
 import com.petshop.petshop_system.entities.Cliente;
@@ -36,13 +37,13 @@ public class MedVetController {
     }
 
     @PostMapping("/login")
-    public String loginVeterinario(@RequestParam String crmv, @RequestParam String senha, Model model) {
+    public String loginVeterinario(@RequestParam String crmv, @RequestParam String senha, Model model, RedirectAttributes redirectAttributes) {
         MedVet veterinario = medVetService.login(crmv, senha); // Método de validação de login
 
         if (veterinario != null) {
             return "redirect:/veterinario/" + crmv; // Redireciona para a página do veterinário com a lista dos animais
         } else {
-            model.addAttribute("error", "CRMV ou senha inválidos");
+            redirectAttributes.addFlashAttribute("error", "CRMV ou senha inválidos");
             return "redirect:/veterinario/login"; // Retorna para o login com erro
         }
     }
